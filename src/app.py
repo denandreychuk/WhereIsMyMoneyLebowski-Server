2d1e036875e0559f232eb0e3547cb627bb99fb5f
Ivan Melnychuk <ivan.melnychuk.me@gmail.com>
2020-09-25 17:14:43 +0300

    GET /app/<id>.

--- src/app.py
+++ src/app.py
@@ -2,16 +2,24 @@ from flask import Flask
 from markupsafe import escape
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
 client = MongoClient()
 db = client.whereismymoney_dev
 
-@app.route('/status/<id>', methods=['GET'])
+@app.route('/app/<id>', methods=['GET'])
 def status(id):
-    query = db.app.find({})
-    for q in query:
-        print(query)
-    return "OK"
+    query = db.app.find_one({"export":escape(id)})
+    print(query)
+    if (query == None):
+        return {
+            "error": 500
+        }
+    return {
+        "name": escape(query['name']),
+        "status": escape(query['status'])
+    }
+
 
 
 
